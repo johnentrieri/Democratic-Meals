9d6c44e0e5dbd06e9df9be1cce8f489887786bae
DESKTOP-ECAGOK7\johnentrieri <john.entrieri@gmail.com>
2019-12-01 12:12:58 -0500

    Added Email Notify API Calls for Meal DB Manager

--- meal-db-manager/meal-db-manager.py
+++ meal-db-manager/meal-db-manager.py
@@ -1,5 +1,24 @@
 import json
 import flask
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def sendEmail(senderEmail, senderPassword,recipient,subject,message):
+    
+    msg = MIMEMultipart()
+    msg['From'] = senderEmail
+    msg['To'] = recipient
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message))
+    
+    mailServer = smtplib.SMTP('smtp.gmail.com', 587)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(senderEmail, senderPassword)
+    mailServer.sendmail(senderEmail, recipient, msg.as_string())
+    mailServer.close()  
 
 app = flask.Flask(__name__)
 
@@ -38,6 +57,47 @@ def getPollData():
     response.headers.add('Access-Control-Allow-Origin', '*')  
 
     return response
+
+@app.route('/notify/', methods=['POST'])
+def notify():
+
+    if (flask.request.form['who'] == 'admin'):
+
+        recipient = user_data['admin']['email']
+        subject = flask.request.form['subject']
+        message = flask.request.form['message']
+
+        sendEmail(user_data['notify']['email'], user_data['notify']['password'],recipient,subject,message)
+
+        response = flask.jsonify( { "status" : "Success", "message" : "Admin Notified"} )
+        response.headers.add('Access-Control-Allow-Origin', '*') 
+
+        return response
+
+    elif (flask.request.form['who'] == 'all'):
+
+        subject = flask.request.form['subject']
+        message = flask.request.form['message']
+        recipient = ""
+
+        for user in user_data['users']:
+
+            recipient += user['email']
+            recipient += ","
+
+        sendEmail(user_data['notify']['email'], user_data['notify']['password'],recipient,subject,message)
+
+        response = flask.jsonify( { "status" : "Success", "message" : "All Users Notified"} )
+        response.headers.add('Access-Control-Allow-Origin', '*') 
+
+        return response
+
+    else:
+        response = flask.jsonify( { "status" : "Fail", "message" : "Unrecognized Recipient"} )
+        response.headers.add('Access-Control-Allow-Origin', '*') 
+
+        return response
+
 
 @app.route('/reset/', methods=['POST'])
 def resetPolls():
